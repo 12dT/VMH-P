eb83707defec490bab0bd347b0610be7879aa29f
12dT <108252269+12dT@users.noreply.github.com>
2026-09-12 14:45:50 +0800

    Delete vmhp/topology directory

--- vmhp/topology/__init__.py
+++
@@ -1,9 +0,0 @@
-from .build_tree import build_tree_topology
-from .build_relations import build_relation_inits
-from .class_centers import compute_class_centers
-
-__all__ = [
-    "build_tree_topology",
-    "build_relation_inits",
-    "compute_class_centers",
-]
